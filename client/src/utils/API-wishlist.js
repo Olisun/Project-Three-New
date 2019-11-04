1b4fcf7fb201092c5234409ef7c2f18066c4df84
Jerry Dudum <jsdudum@gmail.com>
2019-11-04 12:46:51 -0800

    made changes within api routes where duplicate entries were showing up due to a merge conflict earlier

--- client/src/utils/API-wishlist.js
+++ client/src/utils/API-wishlist.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
+
   getItems: function() {
-    return axios.get("/api/items");
+    return axios.get("/api/wishlist");
   },
-  // Gets the book with the given id
+
   getItem: function(id) {
-    return axios.get("/api/items/" + id);
+    return axios.get("/api/wishlist/" + id);
   },
-  // Deletes the book with the given id
+
   deleteItem: function(id) {
-    return axios.delete("/api/items/" + id);
+    return axios.delete("/api/wishlist/" + id);
   },
-  // Saves a book to the database
+
   saveItem: function(wishListData) {
-    return axios.post("/api/items", wishListData);
+    return axios.post("/api/wishlist", wishListData);
   }
 };

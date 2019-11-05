3d712de53d356cffb3ffaf16f13c9e97955db7e4
Jerry Dudum <jsdudum@gmail.com>
2019-11-05 10:41:28 -0800

    added functionality to post budget info to database

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Col, Row } from 'reactstrap';
 import BudgetInput from '../components/BudgetInputForm'
 import WishlistInput from '../components/WishlistInputForm';
-import API from '../utils/API-wishlist'
+import API from '../utils/API-wishlist';
+import budgetAPI from '../utils/API-budget';
 import Rechart from '../components/Chart';
 import DeleteBtn from '../components/DeleteButton';
 import BuyBtn from '../components/BuyButton';
@@ -22,7 +23,9 @@ class Main extends Component {
     utilities: "",
     food: "",
     transportation: "",
-    misc: ""
+    misc: "",
+    totalExpenses: "",
+    allowance: ""
   };
 
   componentDidMount() {
@@ -41,6 +44,12 @@ class Main extends Component {
       )
 
       .catch(err => console.log(err));
+  }
+
+  calculateAllowance = () => {
+    let totalExpenses = (this.state.rentOrMortgage + this.state.utilities + this.state.food + this.state.transportation + this.state.misc);
+    let allowance = (this.state.monthlyIncome - totalExpenses);
+    this.setState({totalExpenses: totalExpenses, allowance: allowance})
   }
 
   deleteItem = id => {
@@ -77,6 +86,22 @@ class Main extends Component {
     }
   };
 
+  handleFormSubmitBudget = event => {
+    event.preventDefault();
+    if (this.state.monthlyIncome) {
+      budgetAPI.saveBudget({
+        Income: this.state.monthlyIncome,
+        Rent: this.state.rentOrMortgage,
+        Utilities: this.state.utilities,
+        Food: this.state.food,
+        Transportation: this.state.transportation,
+        Misc: this.state.misc
+      })
+        .then(res => this.calculateAllowance())
+        .catch(err => console.log(err));
+    }
+  };
+
 
   render() {
     return (
@@ -93,6 +118,7 @@ class Main extends Component {
                   travel={this.state.transportation}
                   miscellaneous={this.state.misc}
                   onChange={this.handleInputChange}
+                  onClick={this.handleFormSubmitBudget}
                 />
               </Col>
               <Col sm={6}>
